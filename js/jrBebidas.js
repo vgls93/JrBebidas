dd78e012d3a509b0a214a708ef38d83383a849a2
Henrique Lopes <159680778+vgls93@users.noreply.github.com>
2025-05-14 11:11:41 -0300

    Add files via upload

---
+++ js/jrBebidas.js
@@ -0,0 +1,6 @@
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+
+menuIcon.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
